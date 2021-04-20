cb6cbbd1aeceaf468569081739541f9e848635f1
apschn <a.pschni@bk.ru>
2021-04-20 21:37:04 +0300

    jopa22

--- src/main/java/com/mafia/mafiabackend/MafiaBackendApplication.java
+++ src/main/java/com/mafia/mafiabackend/MafiaBackendApplication.java
@@ -11,14 +11,4 @@ public class MafiaBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(MafiaBackendApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/player").allowedOrigins("http://localhost:8080");
-            }
-        };
-    }
 }
