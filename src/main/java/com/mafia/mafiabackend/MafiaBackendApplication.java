4f5c587752373bcfa8a083e07c007fdcfab30dfb
apschn <a.pschni@bk.ru>
2021-04-20 21:29:46 +0300

    jopa22

--- src/main/java/com/mafia/mafiabackend/MafiaBackendApplication.java
+++ src/main/java/com/mafia/mafiabackend/MafiaBackendApplication.java
@@ -2,10 +2,23 @@ package com.mafia.mafiabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MafiaBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(MafiaBackendApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/player").allowedOrigins("http://localhost:8080");
+            }
+        };
     }
 }
