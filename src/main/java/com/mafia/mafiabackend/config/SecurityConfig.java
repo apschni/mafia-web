ec295f40bb8eb049c17f341ff5686514f590b656
apschn <a.pschni@bk.ru>
2021-04-20 20:05:56 +0300

    fix CORS problem

--- src/main/java/com/mafia/mafiabackend/config/SecurityConfig.java
+++ src/main/java/com/mafia/mafiabackend/config/SecurityConfig.java
@@ -21,7 +21,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
