6258ccebcf2ab35bf060ea590b36044e8ee1ca61
apschn <a.pschni@bk.ru>
2021-05-14 18:06:35 +0300

    add /gameInfos/{id} method call for finished game

--- src/main/java/com/mafia/mafiabackend/dto/GameInfoDtoResponse.java
+++ src/main/java/com/mafia/mafiabackend/dto/GameInfoDtoResponse.java
@@ -11,14 +11,16 @@ import java.util.List;
 @Schema(description = "Сущность, передаваемая в качестве ответа и содержащая список всех записей по измененной игре" +
         "и индикатор завершения игры")
 public class GameInfoDtoResponse {
+    private Long gameId;
 
     @Schema(description = "Список всех состояний, принадлежащих данной игре")
     private List<GameInfoDto> playerInfos;
 
+    private Boolean gameStarted;
+
     @Schema(description = "Индикатор, показывающий, закончена ли игра")
     private Boolean gameFinished;
 
-    private Long gameId;
+    private Boolean redWin;
 
-    private Boolean gameStarted;
 }
