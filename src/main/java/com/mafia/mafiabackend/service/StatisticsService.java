2415c9407469839408b182aed64f8fbf1831ca69
apschn <a.pschni@bk.ru>
2021-05-14 17:06:41 +0300

    fix zero rating double bug

--- src/main/java/com/mafia/mafiabackend/service/StatisticsService.java
+++ src/main/java/com/mafia/mafiabackend/service/StatisticsService.java
@@ -112,8 +112,9 @@ public class StatisticsService {
                             .playerName(player.getName())
                             .totalWins(totalWins)
                             .totalGames((long) gameInfos.size())
-                            .rating(Double.parseDouble(String.format("%.1f",
-                                    gameInfos.size() == 0 ? 0 : Math.pow(totalWins, 2) / gameInfos.size())))
+                            .rating(gameInfos.size() == 0 || totalWins == 0 ?
+                                    0 : Double.parseDouble(String.format("%.1f",
+                                    Math.pow(totalWins, 2) / gameInfos.size())))
                             .build();
                 })
                 .sorted(Comparator.comparing(x -> ((GameRatingDtoResponse) x).getRating()).reversed())
